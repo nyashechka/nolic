a2c24f21f1ae4804c39c91efe5b07660ef1b5953
nyashechka <anna25white@gmail.com>
2023-05-06 17:52:27 +0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const http = require('http');
 
+
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -8,12 +9,43 @@ const connection = mysql.createConnection({
     database: "chat"
 });
 const server = http.createServer((req,res) =>{
-connection.query(
-    'SELECT * FROM user',
-    function(err,result,fields){
-        console.log(result);
-        res.end(result);
+    if(req.url =="/message"){
+        'SELECT * FROM message',
+        function(err,result,fields){
+        let html = '<html><body><ul>';
+        result.array.forEach(element => {
+            html += `<li>${element.content}</li>`
+        });
+        html += '</ul><form action = "/add" method = "POST"><input type = "text" name ="cont"></form></body></html>'
+        res.writeHead (200, ('Content-type','text/html'));
+        res.end (html);
     }
-)
-})
-server.listen(port)
+ } else if(req.url === "/add"&& req.method ==="POST"){
+        let data = " ";
+        req.on('data', function(chunk){
+            data+= chunk;
+        })
+        req.on('end', function(){
+            let sp = new URLSearchParams(data);
+            let content = sp.get(content);
+            connection.query(
+            'INSERT INTO messages(content, userId,dialogId) VALUES("${content}",1,1);',
+            function(err,result,fields){
+                res.writeHead(302,{'location': '/messages'})
+                res.end();
+           
+            }
+        )})
+    }})
+
+
+// connection.query(
+//     'SELECT * FROM user',
+//     function(err,result,fields){
+//         res.setHeader('Content-type', 'text/html');
+//         res.write(`<${JSON.stringify(result)}`, 'utf-8');
+//         res.end(result);
+//     }
+// )
+// })
+server.listen(3000);
